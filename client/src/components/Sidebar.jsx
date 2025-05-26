1f9d2c8751a9b65fbdcdd019c9f9266c98c3cb1b
kumarabm <kumarabm11@gmail.com>
2025-05-26 16:25:28 +0530

    font colour change

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -41,8 +41,9 @@ export default function Sidebar() {
             <Heart className="w-4 h-4 text-white" />
           </div> */}
           <span className="text-xl font-bold text-slate-800">
-            Healthcare<span className="text-cyan-500">.</span>
-          </span>
+  <span style={{ color: '#7DF9FF' }}>Health</span>care<span className="text-cyan-500">.</span>
+</span>
+
         </div>
       </div>
 
